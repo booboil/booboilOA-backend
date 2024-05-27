6aba5739db82c7bc5c28d1ceef5679112747dc30
zhangxinrong <zhangxinrong@sunsoftai.com>
2024-05-27 12:08:47 +0800

    style: 项目说明优化

--- service-oa/src/main/java/com/booboil/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/booboil/auth/controller/SysRoleController.java
@@ -25,8 +25,6 @@ import java.util.Map;
 public class SysRoleController {
 
     // http://localhost:8800/admin/system/sysRole/findAll
-
-    //注入service
     @Autowired
     private SysRoleService sysRoleService;
 
@@ -46,65 +44,40 @@ public class SysRoleController {
         return Result.ok();
     }
 
-    //查询所有角色
-//    @GetMapping("/findAll")
-//    public List<SysRole> findAll() {
-//        //调用service的方法
-//        List<SysRole> list = sysRoleService.list();
-//        return list;
-//    }
-
-    //统一返回数据结果
     @ApiOperation("查询所有角色")
     @GetMapping("/findAll")
     public Result findAll() {
-        //调用service的方法
         List<SysRole> list = sysRoleService.list();
-
-        //模拟异常效果
         try {
             int i = 10/0;
         } catch (Exception e) {
-            //抛出自定义异常
             throw new GuiguException(20001,"执行了自定义异常处理..");
         }
-
         return Result.ok(list);
     }
 
-    //条件分页查询
-    //page 当前页  limit 每页显示记录数
-    //SysRoleQueryVo 条件对象
     @PreAuthorize("hasAuthority('bnt.sysRole.list')")
     @ApiOperation("条件分页查询")
     @GetMapping("{page}/{limit}")
     public Result pageQueryRole(@PathVariable Long page,
                                 @PathVariable Long limit,
                                 SysRoleQueryVo sysRoleQueryVo) {
-        //调用service的方法实现
         //1 创建Page对象，传递分页相关参数
-        //page 当前页  limit 每页显示记录数
         Page<SysRole> pageParam = new Page<>(page,limit);
-
         //2 封装条件，判断条件是否为空，不为空进行封装
         LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
         String roleName = sysRoleQueryVo.getRoleName();
         if(!StringUtils.isEmpty(roleName)) {
-            //封装 like模糊查询
             wrapper.like(SysRole::getRoleName,roleName);
         }
-
-        //3 调用方法实现
         IPage<SysRole> pageModel = sysRoleService.page(pageParam, wrapper);
         return Result.ok(pageModel);
     }
 
-    //添加角色
     @PreAuthorize("hasAuthority('bnt.sysRole.add')")
     @ApiOperation("添加角色")
     @PostMapping ("save")
     public Result save(@RequestBody SysRole role) {
-        //调用service的方法
         boolean is_success = sysRoleService.save(role);
         if(is_success) {
             return Result.ok();
@@ -113,7 +86,6 @@ public class SysRoleController {
         }
     }
 
-    //修改角色-根据id查询
     @PreAuthorize("hasAuthority('bnt.sysRole.list')")
     @ApiOperation("根据id查询")
     @GetMapping("get/{id}")
@@ -122,7 +94,6 @@ public class SysRoleController {
         return Result.ok(sysRole);
     }
 
-    //修改角色-最终修改
     @PreAuthorize("hasAuthority('bnt.sysRole.update')")
     @ApiOperation("修改角色")
     @PutMapping ("update")
@@ -136,7 +107,6 @@ public class SysRoleController {
         }
     }
 
-    //根据id删除
     @PreAuthorize("hasAuthority('bnt.sysRole.remove')")
     @ApiOperation("根据id删除")
     @DeleteMapping("remove/{id}")
@@ -149,8 +119,6 @@ public class SysRoleController {
         }
     }
 
-    //批量删除
-    // 前端数组 [1,2,3]
     @PreAuthorize("hasAuthority('bnt.sysRole.remove')")
     @ApiOperation("批量删除")
     @DeleteMapping("batchRemove")
